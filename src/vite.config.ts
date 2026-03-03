bae852c4efe81da230448ff23db9297a5b457620
Dhananjay <dhananjaychougule2000@gmail.com>
2026-03-03 08:35:09 +0530

    Fixed image and resume path for GitHub Pages

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/my-portfolio/', // VERY IMPORTANT for GitHub Pages
   plugins: [react()],
-  base: "/my-portfolio/", // Add this line with your repo name
 })
